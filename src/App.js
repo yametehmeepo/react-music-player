c0edb5b55e5f070c6c3b991c1c2182767e259c45
yameteh_meepo@163.com <yameteh_meepo@163.com>
2018-03-07 09:52:05 +0800

    增加键盘控制功能

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './components/header/header';
 import Player from './components/player/player';
+import MusicList from './data/musiclist'
 import $ from 'jquery';
 import 'jplayer';
 import './App.css';
@@ -8,13 +9,20 @@ import './App.css';
 
 
 export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      musiclist: MusicList.list,
+    };
+  }
   componentDidMount(){
+    var musiclist = this.state.musiclist;
     $('#player').jPlayer({//API参考http://www.jplayer.cn/developer-guide.html#jPlayer-event-object
       ready: function(e){
         $(this).jPlayer("setMedia", {
-          mp3: 'http://fs.w.kugou.com/201803051701/92ffe6470cef09066fd4079b1d358754/G012/M01/0A/01/TA0DAFT_BhaAUQwWAEm_q8kA8x0519.mp3',
+          mp3: musiclist[0].file,
           //光辉岁月
-          //http://fs.w.kugou.com/201803051701/92ffe6470cef09066fd4079b1d358754/G012/M01/0A/01/TA0DAFT_BhaAUQwWAEm_q8kA8x0519.mp3
+          //http://dl.stream.qqmusic.qq.com/C400002pKRoX4Qbafa.m4a?vkey=8D3655D62B9B4B5307D6A08160AE7045149C7F1B918C89253156C5EB1EED4483E8ED45987A94765B07576BEF003AACB8ABC7E6CA0280C0D8&guid=2099242356&uin=0&fromtag=66
           //天空之城
           //http://dl.stream.qqmusic.qq.com/C4L0002vsinZ3VJQz8.m4a?vkey=AF9935B9324CE21EFB1FC37131A079A8CD6A26FFF8DD38148E96003FCD38EAA79B9F85410170DDD7E48EF857141D7DD4595410338814B9A0&guid=2099242356&uin=0&fromtag=66
         }).jPlayer('play');
@@ -28,11 +36,12 @@ export default class App extends Component {
       volume: 0.3
     });
   }
+  
   render() {
     return (
       <div className="App">
         <Header />
-        <Player />
+        <Player musiclist={this.state.musiclist} currentIndex={this.state.currentIndex} />
       </div>
     );
   }
--- src/components/player/player.js
+++ src/components/player/player.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Progress from '../progress/progress'
-import MusicList from '../../data/musiclist'
 import $ from 'jquery';
 import 'jplayer';
 import './player.less'
@@ -13,11 +12,12 @@ export default class Player extends React.Component {
 			totaltime: 0,
 			volume: 0,
 			isplayed: true,
-			musiclist: MusicList.list[0]
+			currentIndex: 0,
 		};
 		this.changeprogress = this.changeprogress.bind(this);
 		this.playpausetoggle = this.playpausetoggle.bind(this);
 		this.changevolume = this.changevolume.bind(this);
+		this.changemusic = this.changemusic.bind(this);
 	}
 	componentDidMount(){
 		$('#player').bind($.jPlayer.event.timeupdate, (e) => {//绑定事件
@@ -27,9 +27,27 @@ export default class Player extends React.Component {
 		      volume: e.jPlayer.options.volume*100, //音量范围是0-1
 		    })
 		});	
-		/*setTimeout(()=>{
-			console.log(this.state.volume);
-		},500)*/
+		$(document).keyup((e) => {//添加键盘事件可以控制上一曲下一曲增加音量减少音量
+			switch (e.keyCode) {
+				case 32:
+					this.playpausetoggle();
+					break;
+				case 37:
+					this.changemusic(-1);
+					break;
+				case 38: 
+					this.changevolume(this.state.volume>=90?1:(this.state.volume + 10)/100);
+					break;
+				case 39: 
+					this.changemusic(1);
+					break;
+				case 40: 
+					this.changevolume( this.state.volume<=10?0:(this.state.volume - 10)/100);
+					break;
+				default:
+					break;
+			}
+		})
 	}
 	componentWillUnmount(){
 		$('#player').unbind($.jPlayer.event.timeupdate);
@@ -49,7 +67,6 @@ export default class Player extends React.Component {
 	playpausetoggle(){
 		if(this.state.isplayed){
 			$('#player').jPlayer('pause');
-			
 		}else{
 			$('#player').jPlayer('play');
 		}
@@ -57,8 +74,63 @@ export default class Player extends React.Component {
 			isplayed: !this.state.isplayed,
 		})
 	}
+	changemusic(flag,e){
+		var _this = this;
+		var len = _this.props.musiclist.length-1;
+		var index = _this.state.currentIndex;
+		if(flag>0){
+			index+=1;
+			if(index>len){
+				index = 0;
+			}
+		}else{
+			index-=1;
+			if(index<0){
+				index = len;
+			}
+		}
+		$('.rotateimg').attr('class','rotateimg');
+	    this.setState({
+	      currentIndex: index,
+	      isplayed: true,
+	    });
+	    $('#player').jPlayer('destroy');//销毁jPlayer然后重新实例化jPlayer播放新的歌曲
+	    $('#player').jPlayer({//API参考http://www.jplayer.cn/developer-guide.html#jPlayer-event-object
+	      ready: function(e){
+	        $(this).jPlayer("setMedia", {
+	          mp3: _this.props.musiclist[_this.state.currentIndex].file,
+	          //光辉岁月
+	          //http://dl.stream.qqmusic.qq.com/C400002pKRoX4Qbafa.m4a?vkey=8D3655D62B9B4B5307D6A08160AE7045149C7F1B918C89253156C5EB1EED4483E8ED45987A94765B07576BEF003AACB8ABC7E6CA0280C0D8&guid=2099242356&uin=0&fromtag=66
+	          //天空之城
+	          //http://dl.stream.qqmusic.qq.com/C4L0002vsinZ3VJQz8.m4a?vkey=AF9935B9324CE21EFB1FC37131A079A8CD6A26FFF8DD38148E96003FCD38EAA79B9F85410170DDD7E48EF857141D7DD4595410338814B9A0&guid=2099242356&uin=0&fromtag=66
+	        }).jPlayer('play');
+	      },
+	      ended: function(e){
+	        $(this).jPlayer('play');
+	      },
+	      supplied: "mp3",
+	      wmode: "window",
+	      muted: false,
+	      volume: 0.3
+	    });
+	}
 	render(){
-		var musiclist = this.state.musiclist;
+		var musiclist = this.props.musiclist[this.state.currentIndex];
+		var triggerIndex = this.state.currentIndex%2;
+		var imgclassstr;
+		if(this.state.isplayed){
+			if(triggerIndex){
+				imgclassstr = 'rotateimg restart';
+			}else{
+				imgclassstr = 'rotateimg';
+			}
+		}else{
+			if(triggerIndex){
+				imgclassstr = 'rotateimg restart pause';
+			}else{
+				imgclassstr = 'rotateimg pause';
+			}
+		}
 		return (
 			<div className="content">
 	          <div id="player"></div>
@@ -66,7 +138,7 @@ export default class Player extends React.Component {
 	          	<h2><a href="" className="op">我的私人乐坊 ></a></h2>
 	          	<div className="music-info clearfix">
 	          		<div className="music-cover">
-	          			<img src={musiclist.cover} alt={musiclist.artist} />
+	          			<img src={musiclist.cover} alt={musiclist.artist} className={imgclassstr} />
 	          		</div>
 	          		<div className="music-controller">
 			          	<h3>{musiclist.title}</h3>
@@ -91,9 +163,9 @@ export default class Player extends React.Component {
 			          		/>
 			          	</div>
 			          	<div className="controller-panel clearfix">
-			          		<span className="prev op"></span>
+			          		<span className="prev op" onClick={this.changemusic.bind(null,-1)} ></span>
 			          		<span className={`play ${this.state.isplayed?'':'pause '}op`} onClick={this.playpausetoggle}></span>
-			          		<span className="next op"></span>
+			          		<span className="next op"  onClick={this.changemusic.bind(null,1)}></span>
 			          		<span className="playmode op"></span>
 			          	</div>
 		          	</div>
