c1114e4a81e818b142b37ffc7d442366702f11b2
yameteh_meepo@163.com <yameteh_meepo@163.com>
2018-03-14 14:37:21 +0800

    dangerouslySetInnerHTML={{__html: text}}

--- src/components/lyrics/lyrics.js
+++ src/components/lyrics/lyrics.js
@@ -4,15 +4,19 @@ import './lyricsshow.less'
 
 export default class Lyrics extends Component {
 	componentDidMount(){
-		this.text.innerHTML = this.props.musiclist[this.props.currentIndex].lyrics;
+		//this.text.innerHTML = this.props.musiclist[this.props.currentIndex].lyrics;
 	}
 	render(){
 		var text = this.props.musiclist[this.props.currentIndex].lyrics;
+		//console.log(text);
 		return (
 			<div>
+				{/*
 				<div className="lyricsshow" ref={text => this.text = text}>
 					{text}
 				</div>
+				*/}
+				<div className="lyricsshow" dangerouslySetInnerHTML={{__html: text}}></div>
 				<p className="returnlist"><Link to="/list">返回播放列表</Link></p>
 				<p className="returncontrol"><Link to="/">返回控制界面</Link></p>
 			</div>
